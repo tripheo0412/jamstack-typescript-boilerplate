ce437f48373c91a0c07ef57feb483c9c82c93b78
tripheo0412 <tripheo0412@gmail.com>
2020-02-24 10:20:12 +0200

    improve SEO support for FB, GG,Twitter bots

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,30 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react'
 
-// You can delete this file if you're not using it
+// replace inline css/scss with links
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+}) => {
+  if (process.env.NODE_ENV !== 'production') {
+    return
+  }
+  const headComponents = getHeadComponents()
+
+  // workaround to revert forcefully injected inline styles
+  // described in https://github.com/gatsbyjs/gatsby/issues/1526
+  // takes data-href from <style> tag with inline styles which contains URL to global css file
+  // and transforms it into stylesheet link component
+  const transformedHeadComponents = headComponents.map(node => {
+    if (node.type === 'style') {
+      const globalStyleHref = node.props['data-href']
+      if (globalStyleHref) {
+        return <link href={globalStyleHref} rel="stylesheet" type="text/css" />
+      } else {
+        return node
+      }
+    } else {
+      return node
+    }
+  })
+  replaceHeadComponents(transformedHeadComponents)
+}
