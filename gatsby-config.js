74626367afac6a7e4613c1ff82fe4ec1d98a238e
tripheo0412 <tripheo0412@gmail.com>
2020-02-24 10:25:29 +0200

    add support for sass and less

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
     `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-sass`,
+      options: {
+        // Override the file regex for SASS
+        sassRuleTest: /\.global\.s(a|c)ss$/,
+        // Override the file regex for CSS modules
+        sassRuleModulesTest: /\.mod\.s(a|c)ss$/,
+      },
     },
     {
       resolve: `gatsby-plugin-less`,
