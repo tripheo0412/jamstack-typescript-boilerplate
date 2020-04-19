dae44960e2800262fb240dc1811c1f47d48e0625
SexyBeast0412 <tripheo0412@gmail.com>
2020-04-19 22:24:47 +0700

    add lighthouse ci

--- tasks/lighthouse/detect-pages.sh
+++ tasks/lighthouse/detect-pages.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-packages=$(ls -l ./public/en | grep ^d | awk '{print $9}')
+packages=$(ls -l ./src/pages | grep ^d | awk '{print $9}')
 echo "Searching for pages build by Gatsby"
 echo "All packages: ${packages}"
 
 for package in ${packages[@]}
 do
-  path=$(jq ".ci.collect.url += [\"http://localhost/en/${package}\"]" lighthouserc.json|sponge lighthouserc.json)
+  path=$(jq ".ci.collect.url += [\"http://localhost/${package}\"]" lighthouserc.json|sponge lighthouserc.json)
   echo "${path}"
 done
 
